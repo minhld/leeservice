38997c058c0ef61289d1351eb5e5c50139357074
minhld <minhld38@gmail.com>
2023-10-13 19:01:52 -0400

    add all files

---
+++ src/main/java/com/minhle/leeservice/LeeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.minhle.leeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhle/leeservice/LeeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhle.leeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
