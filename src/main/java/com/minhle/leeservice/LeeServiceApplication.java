f49f70ae19602931fca37cec872d49d86a5e46fb
minhld <minhld38@gmail.com>
2023-10-22 23:58:40 -0400

    correct service name

--- src/main/java/com/minhle/leeservice/LeeServiceApplication.java
+++ src/main/java/com/minhle/leeservice/LeeServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LeeserviceApplication {
+public class LeeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LeeserviceApplication.class, args);
+		SpringApplication.run(LeeServiceApplication.class, args);
 	}
 
 }
