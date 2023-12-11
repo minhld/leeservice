ec5de1cbae086d36233c8a297752ad8421a94f1b
minhld <minhld38@gmail.com>
2023-12-11 00:45:09 -0500

    added swagger ui

---
+++ src/main/java/com/minhle/leeservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.minhle.leeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// @EnableWebSecurity
+public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/token/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .httpBasic(Customizer.withDefaults())
+//                .build();
+//    }
+
+}
