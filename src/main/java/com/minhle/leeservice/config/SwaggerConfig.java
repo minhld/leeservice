c3f2750b14c4d3a7ae97dab32cc095716db16537
minhld <minhld38@gmail.com>
2023-12-10 23:47:04 -0500

    adding mongo & swagger

---
+++ src/main/java/com/minhle/leeservice/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.minhle.leeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Lee Service",
+                "For testing purpose",
+                "1.0.0",
+                "Terms of service",
+                new Contact("Lee's email",
+                        "http://leeservice.com",
+                        "lee@gmail.com"),
+                "MIT",
+                "N/A",
+                Collections.emptyList());
+    }
+
+}
