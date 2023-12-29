8cfe2d5e1747c8ce9ceb5f8ba62276dd2a3c29d7
minhld <minhld38@gmail.com>
2023-12-29 10:20:50 -0500

    added kafka listener

--- src/main/java/com/minhle/leeservice/service/KafkaService.java
+++ src/main/java/com/minhle/leeservice/service/KafkaService.java
@@ -3,6 +3,7 @@ package com.minhle.leeservice.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
@@ -22,12 +23,16 @@ public class KafkaService {
         CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, msg);
         future.whenComplete((result, ex) -> {
             if (ex == null) {
-                log.info("Sent message=[{}] with offset=[{}]", msg, result.getRecordMetadata().offset());
+                log.info("Sent message={} with offset={}", msg, result.getRecordMetadata().offset());
             } else {
-                log.error("Unable to send message=[{}] due to {}", msg, ex.getMessage());
+                log.error("Unable to send message={} due to exception {}", msg, ex.getMessage());
             }
         });
         kafkaTemplate.send(topicName, msg);
     }
 
+    @KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.group-id}")
+    public void listen(String message) {
+        log.info("Received Message {} in group {}", message, "minhle");
+    }
 }
