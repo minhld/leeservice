522243ab43875de7ef5cb39c3aa1d491797e4980
minhld <minhld38@gmail.com>
2024-03-31 00:07:36 -0500

    updated message

--- src/main/java/com/minhle/leeservice/model/Message.java
+++ src/main/java/com/minhle/leeservice/model/Message.java
@@ -1,10 +1,12 @@
 package com.minhle.leeservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class Message {
     String message;
     LocalDateTime time;
