eee6b6ac9db8e010908060415690e9a82328b47f
minhld <minhld38@gmail.com>
2024-04-10 23:59:05 -0500

    updated cassandra configs

--- src/main/java/com/minhle/leeservice/config/CassandraConfig.java
+++ src/main/java/com/minhle/leeservice/config/CassandraConfig.java
@@ -4,20 +4,5 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CassandraConfig extends AbstractCassandraConfiguration {
-
-    @Bean
-    public CassandraClusterFactoryBean cluster() {
-        CassandraClusterFactoryBean cluster =
-                new CassandraClusterFactoryBean();
-        cluster.setContactPoints("127.0.0.1");
-        cluster.setPort(9142);
-        return cluster;
-    }
-
-    @Bean
-    public CassandraMappingContext cassandraMapping()
-            throws ClassNotFoundException {
-        return new BasicCassandraMappingContext();
-    }
+public class CassandraConfig {
 }
