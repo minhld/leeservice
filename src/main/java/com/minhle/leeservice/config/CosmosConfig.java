d80e16d0e449cc455e809b5b51f1504ccfd5ec70
minhld <minhld38@gmail.com>
2024-04-26 23:55:01 -0500

    changed to cosmos

--- src/main/java/com/minhle/leeservice/config/CosmosConfig.java
+++ src/main/java/com/minhle/leeservice/config/CosmosConfig.java
@@ -1,8 +1,7 @@
 package com.minhle.leeservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CassandraConfig {
+public class CosmosConfig {
 }
