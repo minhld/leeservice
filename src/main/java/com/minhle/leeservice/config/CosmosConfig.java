0bf6f71d7e86d356192f7910ba894500aff2a2f5
minhld <minhld38@gmail.com>
2024-05-05 23:59:14 -0500

    added cosmos support

--- src/main/java/com/minhle/leeservice/config/CosmosConfig.java
+++ src/main/java/com/minhle/leeservice/config/CosmosConfig.java
@@ -1,8 +1,28 @@
 package com.minhle.leeservice.config;
 
+import com.azure.data.cosmos.CosmosKeyCredential;
+import com.microsoft.azure.spring.data.cosmosdb.config.AbstractCosmosConfiguration;
+import com.microsoft.azure.spring.data.cosmosdb.config.CosmosDBConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CosmosConfig {
+public class CosmosConfig extends AbstractCosmosConfiguration {
+    @Value("${azure.cosmosdb.uri}")
+    private String uri;
 
+    @Value("${azure.cosmosdb.key}")
+    private String key;
+
+    @Value("${azure.cosmosdb.database}")
+    private String dbName;
+
+    private CosmosKeyCredential cosmosKeyCredential;
+
+    @Bean
+    public CosmosDBConfig getConfig() {
+        this.cosmosKeyCredential = new CosmosKeyCredential(key);
+        return CosmosDBConfig.builder(uri, this.cosmosKeyCredential, dbName).build();
+    }
 }
