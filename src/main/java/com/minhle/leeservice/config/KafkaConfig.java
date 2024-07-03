3d856b34f0e716def1471a5c80861897cc901f82
minhld <minhld38@gmail.com>
2024-07-03 12:07:16 +0700

    ready to use kstream

--- src/main/java/com/minhle/leeservice/config/KafkaConfig.java
+++ src/main/java/com/minhle/leeservice/config/KafkaConfig.java
@@ -2,10 +2,15 @@ package com.minhle.leeservice.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
@@ -18,6 +23,8 @@ public class KafkaConfig {
     private String bootstrapAddress;
     @Value(value = "${spring.kafka.topic}")
     private String topicName;
+    @Value(value = "${spring.kafka.streams.state.dir}")
+    private String stateStoreLocation;
 
     @Bean
     public KafkaAdmin kafkaAdmin() {
@@ -27,7 +34,20 @@ public class KafkaConfig {
     }
 
     @Bean
-    public NewTopic topic1() {
+    public NewTopic newTopic() {
         return new NewTopic(topicName, 1, (short) 1);
     }
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    KafkaStreamsConfiguration kStreamsConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "lee-services");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        // configure the state location to allow tests to use clean state for every run
+        props.put(StreamsConfig.STATE_DIR_CONFIG, stateStoreLocation);
+        return new KafkaStreamsConfiguration(props);
+    }
+
 }
