bd1d34338f38e0affe9c1e1c7f773047a149c322
minhld <minhld38@gmail.com>
2024-07-10 14:42:57 +0700

    minor updates

--- src/main/java/com/minhle/leeservice/controller/KafkaController.java
+++ src/main/java/com/minhle/leeservice/controller/KafkaController.java
@@ -8,8 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @ConditionalOnProperty(value = "spring.kafka.enabled", matchIfMissing = true)
 public class KafkaController {
