6facbb814ca4bc656edd786b472670abcea68f04
minhld <minhld38@gmail.com>
2024-10-11 01:51:56 -0500

    fixed avro

--- src/main/java/com/minhle/leeservice/LeeServiceApplication.java
+++ src/main/java/com/minhle/leeservice/LeeServiceApplication.java
@@ -2,8 +2,10 @@ package com.minhle.leeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.schema.client.EnableSchemaRegistryClient;
 
 @SpringBootApplication
+@EnableSchemaRegistryClient
 public class LeeServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/minhle/leeservice/config/KafkaProducerConfig.java
+++ src/main/java/com/minhle/leeservice/config/KafkaProducerConfig.java
@@ -9,12 +9,13 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.stream.schema.client.ConfluentSchemaRegistryClient;
+import org.springframework.cloud.stream.schema.client.SchemaRegistryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +25,8 @@ import java.util.Map;
 public class KafkaProducerConfig {
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
+    @Value("${spring.kafka.producer.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
 
     @Bean
     public ProducerFactory<String, Employee> producerFactory() {
@@ -37,5 +40,12 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, Employee> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public SchemaRegistryClient schemaRegistryClient() {
+        ConfluentSchemaRegistryClient client = new ConfluentSchemaRegistryClient();
+        client.setEndpoint(schemaRegistryUrl);
+        return client;
     }
 }
