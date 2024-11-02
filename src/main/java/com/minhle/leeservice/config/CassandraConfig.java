80ece7eab5cdb5234aa2d88b2c0c173d29d742b6
minhld <minhld38@gmail.com>
2024-11-01 22:56:32 -0500

    added cassandra configs

--- src/main/java/com/minhle/leeservice/config/CassandraConfig.java
+++ src/main/java/com/minhle/leeservice/config/CassandraConfig.java
@@ -1,7 +1,13 @@
 package com.minhle.leeservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CassandraConfig {
+    @Value(value = "${spring.data.cassandra.cluster}")
+    private String cluster;
+    @Value(value = "${spring.data.cassandra.port}")
+    private String port;
+
 }
