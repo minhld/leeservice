aa2c0588aea7f4bec6614411a0dbb0c93f4fdba8
minhld <minhld38@gmail.com>
2025-02-15 23:59:07 -0600

    added minor change

--- src/main/java/com/minhle/leeservice/config/CassandraConfig.java
+++ src/main/java/com/minhle/leeservice/config/CassandraConfig.java
@@ -1,7 +1,6 @@
 package com.minhle.leeservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
